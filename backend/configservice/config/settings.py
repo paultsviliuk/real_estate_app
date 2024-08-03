7fe8c9fc0ac38e89bff834299584c1ae45c38946
Paul <paultsvilyuk@gmail.com>
2024-08-03 18:44:27 +0200

    added mysql db to config service.

--- backend/configservice/config/settings.py
+++ backend/configservice/config/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'config',
+        'USER': 'microservice',
+        'PASSWORD': 'microservice',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
